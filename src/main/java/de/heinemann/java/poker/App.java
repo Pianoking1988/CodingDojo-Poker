c0697b4339b53e237611de4032509415350f4532
Oliver Heinemann <oliver.schmitz.informatik+github@googlemail.com>
2019-01-05 15:24:20 +0100

    Start repository

---
+++ src/main/java/de/heinemann/java/poker/App.java
@@ -0,0 +1,13 @@
+package de.heinemann.java.poker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
